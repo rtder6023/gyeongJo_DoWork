35f68edee76f7c3b7609b2ff41f7adcc777e9914
rtder6023 <rtder6023@gmail.com>
2026-06-12 09:18:43 +0900

    Work 경삣삐

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
       setTodoData(newTodoData);
       localStorage.setItem("todoData", JSON.stringify(newTodoData));
     },
-    [todoData]
+    [todoData],
   );
 
   const handleSubmit = (e) => {
@@ -48,8 +48,8 @@ export default function App() {
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
-          <h1>할 일 목록</h1>
-          <button onClick={handleRemoveClick}>Delete All</button>
+          <h1>경조야 1 해라</h1>
+          <button onClick={handleRemoveClick}>모든 경조 삭제</button>
         </div>
 
         <List
